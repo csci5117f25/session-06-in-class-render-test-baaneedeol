9261c85e4b8f5f3d57c06e19225230cf1aa578a4
Baanee Singh <baaneesingh@infra04-wg012.eduroam.wireless.umn.edu>
2025-09-25 08:56:53 -0500

    db connection issues

---
+++ db.py
@@ -0,0 +1,33 @@
+import os
+from contextlib import contextmanager
+from flask import current_app
+import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import DictCursor
+
+pool = None
+
+def setup(app):
+    global pool
+    DATABASE_URL = os.environ['DATABASE_URL']
+    current_app.logger.info("Creating DB connection pool")
+    pool = ThreadedConnectionPool(1, 20, dsn=DATABASE_URL, sslmode='require')
+
+@contextmanager
+def get_db_connection():
+    conn = pool.getconn()
+    try:
+        yield conn
+    finally:
+        pool.putconn(conn)
+
+@contextmanager
+def get_db_cursor(commit=False):
+    with get_db_connection() as conn:
+        cur = conn.cursor(cursor_factory=DictCursor)
+        try:
+            yield cur
+            if commit:
+                conn.commit()
+        finally:
+            cur.close()
--- server.py
+++ server.py
@@ -6,46 +6,18 @@ import psycopg2
 from psycopg2.pool import ThreadedConnectionPool
 from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
+import db
 
 load_dotenv()  
 
 app = Flask(__name__)
 
-@app.before_request
-def init_db_pool():
-    setup(app)
+db.setup(app)
 
 # @app.route('/')
 # @app.route('/<name>')
 # def hello(name=None):
 #     return render_template('guest_form', name=name)
-
-pool = None
-
-def setup(app):
-    global pool
-    DATABASE_URL = os.environ['DATABASE_URL']
-    app.logger.info("Creating DB connection pool")
-    pool = ThreadedConnectionPool(1, 20, dsn=DATABASE_URL, sslmode='require')
-
-@contextmanager
-def get_db_connection():
-    conn = pool.getconn()
-    try:
-        yield conn
-    finally:
-        pool.putconn(conn)
-
-@contextmanager
-def get_db_cursor(commit=False):
-    with get_db_connection() as conn:
-        cur = conn.cursor(cursor_factory=DictCursor)
-        try:
-            yield cur
-            if commit:
-                conn.commit()
-        finally:
-            cur.close()
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
@@ -53,14 +25,14 @@ def index():
         name = request.form["name"]
         message = request.form["message"]
 
-        with get_db_cursor(commit=True) as cur:
+        with db.get_db_cursor(commit=True) as cur:
             cur.execute(
                 "INSERT INTO guestbook (name, message) VALUES (%s, %s);",
                 (name, message),
             )
         return redirect("/") 
 
-    with get_db_cursor() as cur:
+    with db.get_db_cursor() as cur:
         cur.execute("SELECT name, message FROM guestbook ORDER BY created_at DESC;")
         guests = cur.fetchall()
 
