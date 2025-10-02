02d32fbf1c87e06ecf97a2670209b2689c58baff
Baanee Singh <baaneesingh@infra04-wg012.eduroam.wireless.umn.edu>
2025-10-02 09:03:12 -0500

    auth config

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 from contextlib import contextmanager
 from flask import current_app
@@ -7,24 +7,45 @@ from psycopg2.pool import ThreadedConnectionPool
 from psycopg2.extras import DictCursor
 from dotenv import load_dotenv
 import db
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
 
-load_dotenv()  
+# Load .env file
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET", "fallback-secret")  # safer
 
 with app.app_context():
     db.setup(app)
-    
-# @app.route('/')
-# @app.route('/<name>')
-# def hello(name=None):
-#     return render_template('guest_form', name=name)
 
-@app.route('/', methods = ["GET", "POST"])
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=os.environ.get("AUTH0_CLIENT_ID"),
+    client_secret=os.environ.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={"scope": "openid profile email"},
+    server_metadata_url=f'https://{os.environ.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect(url_for("index"))
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form["name"]
-        message = request.form["message"]
+        name = request.form.get("name", "Anonymous")
+        message = request.form.get("message", "")
 
         with db.get_db_cursor(commit=True) as cur:
             cur.execute(
@@ -37,4 +58,20 @@ def index():
         cur.execute("SELECT name, message FROM guestbook ORDER BY created_at DESC;")
         guests = cur.fetchall()
 
-    return render_template("guest_form.html", guests=guests)
+    # define name = None to avoid NameError in GET
+    return render_template("guest_form.html", name=None, guests=guests)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + os.environ.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("index", _external=True),
+                "client_id": os.environ.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
